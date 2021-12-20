9f77dc7238d91f9ccfc631834f421e9fd6fe26c4
ugurozcelik1 <95427836+ugurozcelik1@users.noreply.github.com>
2021-12-20 19:33:21 +0300

    Add files via upload

---
+++ Java101/src/Faktoriyel.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Faktoriyel {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz: ");
+        int a = input.nextInt();
+        System.out.print("İkici sayıyı giriniz: ");
+        int b = input.nextInt();
+
+        int total1 = 1, total2 = 1, total0 = 1;
+
+        for (int i = 1; i <= a; i++){
+            total1 *= i;
+        }
+        for (int ii = 1; ii <= b; ii++){
+            total2 *= ii;
+        }
+        for (int iii = 1; iii <= (a-b); iii++){
+            total0 *= iii;
+        }
+        System.out.println("C(" + a + "," + b + ")= " + total1/(total2*total0));
+    }
+}
