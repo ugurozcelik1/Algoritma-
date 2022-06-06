409b8a2838444cb1120d099afa36647a7adb358f
ugr7@hotmail.com <95427836+ugurozcelik1@users.noreply.github.com>
2022-06-06 19:29:28 +0300

    Delete React First Project directory

--- React First Project/app.js
+++
@@ -1,5 +0,0 @@
-import getData from "./index.js"
-
-const result = await getData(2);
-
-console.log(result);
--- React First Project/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-export default async  function (number){
-    const {data: user} = await axios("https://jsonplaceholder.typicode.com/users/" + number)  
-    const {data: post} = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + number)
-    
-    const result = Object.assign(user, post)
-    
-    return result;
-}
